8cc5886c53b37bd14640da051730de927d46379e
exactor <kyzya96@gmail.com>
2020-07-06 00:36:00 +0300

    add log file

--- tg_bot.go
+++ tg_bot.go
@@ -20,6 +20,15 @@ var mainKeyboard = tgbotapi.NewReplyKeyboard(
 )
 
 func main() {
+
+	f, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TokenTg"))
 	if err != nil {
 		log.Panic(err)
